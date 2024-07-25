c663f204033d6a107931726e65d15d851b010e9b
LAB-USR-LNORTE <LAB-USR-LNORTE@UTP311535.grupoutp.net>
2024-07-25 13:09:38 -0500

    empezando

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author LAB-USR-LNORTE
+ */
+
+import java.util.Scanner;
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        
+        Scanner lector= new Scanner (System.in);
+        
+        System.out.println("Pulse enter para empezar con el programa");
+        lector.nextLine();
+        System.out.println("Hello World!");
+        
+    }
+}
